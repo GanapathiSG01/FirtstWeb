65039b8782902ff781d11f261eb8cef1cfd9b31c
GanapathiSG01 <108076937+GanapathiSG01@users.noreply.github.com>
2022-06-23 21:46:47 +0530

    Add files via upload
    Direct

---
+++ APIWorkOut/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APIWorkOut.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace APIWorkOut.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        //[Route("api/Login/Username/Password")]
+        public IActionResult Login(string Username,string Password)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(new UserModel { Username=Username,Password=Password});
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              null,
+              expires: DateTime.Now.AddMinutes(2),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+
+            //Validate the User Credentials    
+            //Demo Purpose, I have Passed HardCoded User Information    
+            if (login.Username == "Jignesh")
+            {
+                user = new UserModel { Username = "Jignesh Trivedi", EmailAddress = "test.btest@gmail.com" };
+            }
+            return user;
+        }
+
+    }
+}
---
+++ APIWorkOut/Controllers/ValuesController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIWorkOut.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("GetCustomerDetails")]
+        [Authorize]
+        public ActionResult<IEnumerable<object>> GetCustomerDetails()
+        {
+            List<Employee> lstEmployee = new List<Employee>
+            { new Employee { id=1, Name="Gana",DepId=1,Salary=1000 },
+                new Employee { id = 2, Name = "Mani", DepId = 2,Salary=2000 },
+            new Employee { id=3, Name="Praba",DepId=3,Salary=2500 },
+            new Employee { id=4, Name="Parvesh",DepId=4,Salary=3000 },
+            new Employee { id=5, Name="Ajay",DepId=1,Salary=3000 },
+            new Employee { id=6, Name="Jadeja",DepId=2,Salary=4000 }
+            };
+
+            List<Department> lstDepartment = new List<Department>
+            {
+                new Department{ id=1,Name="HR" },
+                new Department{ id=2,Name="Sales"} 
+            };
+
+            var res = lstEmployee.GroupJoin(lstDepartment,
+                emp => emp.DepId,
+                dep => dep.id,
+            (Emp, Dep) => new { EMP = Emp, DEP = Dep }).SelectMany(DepE => DepE.DEP.DefaultIfEmpty(),
+            (ABC, DEP) => new { ABC.EMP.id, ABC.EMP.Name, DEPID =(DEP == null?0: DEP.id), DEPNAME = (DEP == null ? "" : DEP.Name) }
+            ).ToArray();
+
+
+            var res1 = (from emp in lstEmployee
+                        join dep in lstDepartment on emp.DepId equals dep.id
+into EmpWithDepartment
+                        from EmpWithDep in EmpWithDepartment.DefaultIfEmpty()
+                        select new { emp.id, emp.Name, DepId = (EmpWithDep == null) ? 0 : EmpWithDep.id, DepartmentName = (EmpWithDep == null) ? "" : EmpWithDep.Name }).ToList();
+
+
+            var departmentwiseHighSalary = lstEmployee.Join(lstDepartment, em => em.DepId, de => de.id,
+            (x, y) => new { A = x, B = y }
+                ).GroupBy(a =>new { a.B.id,a.B.Name }).Select(a => new { Employee = a.OrderByDescending(c => c.A.Salary).FirstOrDefault() }).ToList();
+
+            return res;
+        }
+
+        // GET api/values
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+
+    public class Employee {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int DepId { get; set; }
+        public int Salary { get; set; }
+    }
+    public class Department {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
